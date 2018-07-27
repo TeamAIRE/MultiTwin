5532e198702e3caefb6da42c2d561d74e054108a
Team AIRE <31657245+TeamAIRE@users.noreply.github.com>
2018-07-27 12:47:23 +0200

    Delete config.sh

--- config.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-CWD=$(pwd)
-echo -e "\nexport PATH=$CWD/python-scripts:\$PATH" >> ~/.profile
-echo -e "\nexport PATH=$CWD/BlastProg:\$PATH" >> ~/.profile
-echo -e "\nexport PYTHONPATH=$CWD/python-scripts:\$PYTHONPATH" >> ~/.profile
-cd BlastProg/
-make
-
